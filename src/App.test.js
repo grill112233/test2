f937b499cf7481fdf989841860a632e12b346481
grill112233 <grill_0822553047@hotmail.com>
2020-11-25 04:15:31 +0700

    mmmmV7

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ToggleComponent from './ToggleComponent';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,4 +15,18 @@ describe('ToggleComponent', () => {
      const test = true;
      expect(test).toBe(true);
   });
+});
+
+// it('Should be false', () => {
+//   const test = 'Shubham';
+//   expect(test).toBe('Ankit');
+// });
+
+Enzyme.configure({ adapter: new Adapter() });
+describe('ToggleComponent', () => {
+  it('should show the text', () => {
+  const toggleInstance = shallow(<ToggleComponent />);
+  const element = toggleInstance.find('div div');
+  expect(element.text()).toBe('This will be toggled');
+ });
 });
