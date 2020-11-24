5e9b11493f0fa2ef94f2ed43338310a5610c40cb
grill112233 <grill_0822553047@hotmail.com>
2020-11-25 04:55:32 +0700

    mmmmV9

--- src/App.js
+++ src/App.js
@@ -25,4 +25,32 @@ class App extends Component {
 
 }
 
+const bodyParser = require('body-parser');
+const cors = require('@robertoachar/express-cors');
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const { catchAll, notFound } = require('./error');
+
+const app = express();
+const userRouter = require('./user/user.router');
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'It works!' });
+});
+
+app.use('/api/users', userRouter);
+
+app.use(notFound);
+app.use(catchAll);
+
+module.exports = app;
+
 export default App;
